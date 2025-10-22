ba832860c7cede4609f9bb9f72eeefba91a9b1bc
Boris <borisgomez141@gmail.com>
2025-10-22 18:42:07 -0300

    arreglo import

--- pre-entrega/src/App.jsx
+++ pre-entrega/src/App.jsx
@@ -1,7 +1,6 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
 import { ItemDetailContainer } from './components/itemDetailContainer/ItemDetailContainer';
 import { CartProvider } from './context/CartContext/CartProvider';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
